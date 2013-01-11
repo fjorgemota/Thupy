0fa196457661b8c04246dfdb2a4d8265ac474141
Fernando Jorge Mota <f.j.mota13@gmail.com>
2013-01-11 21:17:11 -0200

    Added support to implementation of support to various file types

---
+++ filestypes/BaseType.py
@@ -0,0 +1,17 @@
+'''
+Created on 23/12/2012
+
+@author: fernando
+'''
+from utils.BasicClass2
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
---
+++ filestypes/PythonPages.py
@@ -0,0 +1,5 @@
+'''
+Created on 23/12/2012
+
+@author: fernando
+'''
