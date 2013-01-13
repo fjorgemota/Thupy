596ba9cbae9926041db91d482602644466361ac0
Fernando <f.j.mota13@gmail.com>
2013-01-13 19:35:44 -0200

    Added support to Async Sockets

--- sockets/AsyncSocket.py
+++ sockets/AsyncSocket.py
@@ -10,17 +10,18 @@ from BaseSocket import SocketConfig
 from sockets.BaseSocket import SocketType
 class AsyncSocket(NormalSocket, BaseAsyncSocket):
     '''Implements a AsyncSocket using handlers defined by the AsyncSocket class'''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Implements constructor to construct queues'''
-        super(AsyncSocket, self).__init__()
-        self.__write_messages = []
-        self.__read_messages = Queue()
+        super(AsyncSocket, self).__init__(*args, **kwargs) #Call super class to initialize other variables
+        self.__write_messages = [] #List with characters to be writed to the socket
+        self.__read_messages = Queue() #Queue with readed messages
+        self.__accepted_sockets = Queue() #Queue with accepted sockets
     def canRead(self):
         '''It's always readable'''
-        return True
+        return True #It can read in any time..
     def canWrite(self):
         '''If has messages to be sended, return True, otherwise False'''
-        return bool(self.__write_messages)
+        return bool(self.__write_messages) #It have new characters?
     def write(self, msg):
         '''Put each character in the queue'''
         self.__write_messages.extend(msg)
@@ -31,19 +32,24 @@ class AsyncSocket(NormalSocket, BaseAsyncSocket):
         if buf < 0:
             buf = self.__read_messages.qsize()
         for _ in xrange(0, buf):
-            result.append(self.__read_messages.get(blocking))
+            result.append(self.__read_messages.get(blocking)) #Await a new character
             self.__read_messages.task_done()
         return "".join(result)
     def handle_read(self):
+        if self.getConfig(SocketConfig.MODE) == SocketType.SERVER: #It can't read from a socket that is a...server
+            return #Just return
         map(self.__read_messages.put_nowait, super(AsyncSocket, self).read())
     def handle_write(self):
         '''Send messages from the buffer..'''
-        if not self.__write_messages or not self.:
+        if not self.__write_messages or self.getConfig(SocketConfig.MODE) == SocketType.SERVER:#It can't write if not have messages or the socket is a...server
             return
         self.__write_messages = self.__write_messages[self.__sock.send("".join(self.__write_messages)):]
     def handle_accept(self):
         '''Accepts...But not in this socket (?)'''
-        
+        if self.getConfig(SocketConfig.MODE) == SocketType.CLIENT:#It can't accept of a client
+            return #Just return
+        self.__accepted_sockets.put_nowait(AsyncSocket(self.__sock.accept())) #Accept and put on queue..
+    
     
         
     
