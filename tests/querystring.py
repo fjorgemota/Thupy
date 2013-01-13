8621878a26664e688d46c9539bad7d430a9fecac
Fernando <f.j.mota13@gmail.com>
2013-01-13 19:34:24 -0200

    Added querystring processor with support to lists and sub-dictionaries :D

--- tests/querystring.py
+++ tests/querystring.py
@@ -9,14 +9,68 @@ def validateKey(q):
 	return q[0]
 def separateKey(q):
 	return q.split("=",1)
+def convertToList(d):
+	is_list = True #Indicates if can be a list
+	keys = []
+	for key in d.iterkeys():
+		if not key.isdigit() and key:
+			is_list = False #It can't be a list..
+			break
+		elif key.isdigit() and key:
+			keys.append(int(key))
+	r = {}
+	for key, val in d.iteritems():
+		if isinstance(val, dict): #It's a dictionary!
+			r[key] = convertToList(val)
+		else:
+			r[key] = val
+	if is_list and keys:#It have keys?
+		keys.sort()#Sorts the list
+		if keys[0] != 0:
+			#If the first key is different from zero, it can't be a list..
+			return r
+		#Process to become a list
+		l = []
+		for key in keys:
+			l.insert(key, r[str(key)])
+		return l 
+	return r
+def adaptGet(itens):
+	r = {}
+	for key, val in itens:
+		brackets = key.split("[")
+		if brackets:
+			#It's a Array, and it's recursive
+			children = r #Children is just a pointer to r
+			c = 0 #Initialize at zero
+			l = len(brackets)-1 #Length-1 to detect end
+			is_end = lambda: c==l #Indicate if ends
+			for bracket in brackets:
+				key_child = bracket.split("]",1)[0]
+				if not key_child and c>0:
+					key_child = str(len(children))
+				children[key_child] = children.get(key_child, {})
+				if not is_end():
+					children = children[key_child]#Replaces the pointer
+				else:
+					children[key_child] = val
+				c += 1
+		else:
+			#It's not a array \o/
+			r[key] = val
+	return convertToList(r)
 def processWithMap(qs):
-	return dict(map(remapGET, map(separateKey, qs.split("&"))))
+	return adaptGet(remapGET(separateKey(q)) for q in qs.split("&"))
+	
 detectRE = re.compile("&?([^&=]+)=?([^&]*)").findall
 def processWithRE(qs):
 	#return dict(map(remapGET, re.findall("&?([^&=]+)=?([^&]*)", qs)))
-	return dict(map(remapGET, detectRE(qs)))
+	return adaptGet(remapGET(q) for q in detectRE(qs))
 	#return dict(remapGET(q) for q in detectRE(qs))
-qs = createQueryString(1000)
+#qs = createQueryString(10)
+qs = "teste[0][0][]=rs&teste[0][0][]=oi"
+print processWithRE(qs) == processWithMap(qs)
+print processWithMap(qs)
 num_requests = 10000
 print "Rodando %d requisicoes.."%num_requests
 #print qs
@@ -31,4 +85,4 @@ def benchmark(func):
 #print processWithRE(qs)
 processWithURLParse.__name__ = "processWithURLParse"
 funcs = [processWithMap, processWithRE, processWithURLParse]
-map(benchmark, funcs)
+#map(benchmark, funcs)
