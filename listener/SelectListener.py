c72c5adbf36c5840ec63afe8ebbf638eea893f70
Fernando Jorge Mota <f.j.mota13@gmail.com>
2013-01-15 19:41:28 -0200

    Added queued based operations

--- listener/SelectListener.py
+++ listener/SelectListener.py
@@ -4,8 +4,8 @@ Created on 13/01/2013
 @author: fernando
 '''
 from BaseListener import BaseListener, ListenerMode
-from utils.itertools import itertools
-import select, threading
+from utils.BasicClass import enum
+import select, threading, Queue
 from sockets.BaseSocket import isAsync, SocketConfig, SocketType
 def getFileNO(s):
     '''Just get File Number to use with Sockets =P'''
@@ -13,63 +13,101 @@ def getFileNO(s):
 def getSockets(sockets):
     '''Return a dict with file numbers as keys and sockets objects as values'''
     return dict(zip(map(getFileNO, sockets), sockets)) 
+class SelectActions(enum):
+    ADD = None
+    REMOVE = None
 class SelectListener(BaseListener):
     '''Listener that use Select functions to detect events'''
     def __init__(self):
         '''Constructs the listener, initializing variables such as readed sockets, writed sockets and etc...'''
         self.__read_sockets__ = []
         self.__write_sockets__ = []
-        self.__modify_sockets__ = threading.Lock()
-        self.__result_events__ = threading.Lock()
         self.__read_events__ = []
         self.__write_events__ = []
-    def poll(self):
+        self.__timeout__ = None
+        self.__modes__ = {ListenerMode.READ:self.__read_sockets__, ListenerMode.WRITE:self.__write_sockets__}
+        self.__new_sockets__ = []
+        self.__new_modify_sockets__ = threading.Lock()
+        self.__actions__ = Queue.Queue() 
+    def poll(self, blocking=False):
         '''Returns a list with sockets that have events to read and write'''
+        if blocking:
+            event = threading.Event()
+            self.__new_modify_sockets__.acquire()
+            self.__new_sockets__.append(event)
+            self.__new_modify_sockets__.release()
+            event.wait()#Block..
+            event.clear()
+            self.__new_modify_sockets__.acquire()
+            self.__new_sockets__.remove(event)
+            self.__new_modify_sockets__.release()
         return self.__read_events__+self.__write_events__
     def add(self, sock, mode):
         '''Add the socket to the listener'''
         assert mode in ListenerMode, "Mode invalid"
-        add = [self.__read_sockets__.append, self.__write_sockets__.append][mode==ListenerMode.WRITE]
-        self.__modify_sockets__.acquire()
-        add(sock)
-        self.__modify_sockets__.release()
-    def remove(self, sock, mode):
+        self.__actions__.put_nowait([SelectActions.ADD, sock, mode])#Add to the queue
+    def remove(self, sock):
         '''Remove the socket from the listener'''
-        assert mode in ListenerMode, "Mode invalid"
-        remove = [self.__read_sockets__.remove, self.__write_sockets__.remove][mode==ListenerMode.WRITE]
-        self.__modify_sockets__.acquire()
-        remove(sock) #Remove, or raises a error if not exists
-        self.__modify_sockets__.release()
+        for mode in self.__modes__.keys():
+            self.__actions__.put_nowait([SelectActions.REMOVE, sock, mode]) #Add to the queue
     def modify(self, sock, mode):
         '''Modify the listener mode of the socket'''
         assert mode in ListenerMode, "Mode invalid"
-        self.remove(sock, [ListenerMode.READ, ListenerMode.WRITE][mode == ListenerMode.READ])
+        self.remove(sock) #Remove the other event (?)
         self.add(sock, mode)
-    def getToRead(self):
+    def getToRead(self, blocking=False):
         '''Return a list with sockets that can be readed'''
+        if blocking:
+            event = threading.Event()
+            self.__new_modify_sockets__.acquire()
+            self.__new_sockets__.append(event)
+            self.__new_modify_sockets__.release()
+            event.wait()#Block..
+            event.clear()
+            self.__new_modify_sockets__.acquire()
+            self.__new_sockets__.remove(event)
+            self.__new_modify_sockets__.release()
         return self.__read_events__
-    def getToWrite(self):
-        '''Return a list with sockets that can be readed''',
+    def getToWrite(self, blocking=False):
+        '''Return a list with sockets that can be readed'''
+        if blocking:
+            event = threading.Event()
+            self.__new_modify_sockets__.acquire()
+            self.__new_sockets__.append(event)
+            self.__new_modify_sockets__.release()
+            event.wait()#Block..
+            event.clear()
+            self.__new_modify_sockets__.acquire()
+            self.__new_sockets__.remove(event)
+            self.__new_modify_sockets__.release()
         return self.__write_events__
+    def getTimeout(self):
+        return self.__timeout__
+    def setTimeout(self, timeout=-1):
+        self.__timeout__ = [timeout, None][timeout<0]
     def run(self):
         '''Run the thread listener in a separate thread, and send the data to other channels (if not sync)'''
         while True:
             #Runs infinitely
-            self.__modify_sockets__.acquire()
+            while True:
+                try:
+                    action, sock, mode = self.__actions__.get_nowait() #Get a item on the queue
+                except Queue.Empty:
+                    break #Or break if empty =P
+                if action == SelectActions.ADD:
+                    self.__modes__[mode].append(sock)
+                elif action == SelectActions.REMOVE and sock in self.__modes__[mode]:
+                    self.__modes__[mode].remove(sock)
             read_sockets = getSockets(self.__read_sockets__)#Organize it..xD
             write_sockets = getSockets(self.__write_sockets__) #Organize it..xD
-            self.__modify_sockets__.release()
-            to_read, to_write, to_except = select.select(read_sockets.keys(), write_sockets.keys(), read_sockets.keys()+write_sockets.keys())
+            to_read, to_write, to_except = select.select(read_sockets.keys(), write_sockets.keys(), read_sockets.keys()+write_sockets.keys(), self.__timeout__)
             read_events = []
             write_events = []
             for sockno in to_read:
                 #If socket is async...we have to see if it's a client, if it is, we call handle_read, if it's a server, we call handle_accept, otherwise, we publish it in a list
                 sock = read_sockets[sockno]
                 if isAsync(sock): #If async, call the methods..
-                    if sock.getConfig(SocketConfig.MODE) == SocketType.CLIENT:
-                        sock.handle_read()
-                    else:
-                        sock.handle_accept()
+                    sock.handle_read_event()
                 else:
                     read_events.append(sock)
             for sockno in to_write:
@@ -85,5 +123,7 @@ class SelectListener(BaseListener):
                 sock.close() #If not exist, we have a big error in question of:::: Logic =P
             self.__read_events__ = read_events #Just replace the list of sockets that can be readed (PS: It's thread-safe)
             self.__write_events__ = write_events #Just replace the list of sockets that can be writed (PS: It's thread-safe)
-            
+    @staticmethod
+    def getSupportedPoints():
+        return [-1, 10][hasattr(select, "select")] #In some implementations of Python that can be not exist =P
             
